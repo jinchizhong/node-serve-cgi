aa56ddc8ac3d64606e076fbd503d9cbe5d796079
jinchizhong <jinchizhong@kingsoft.com>
2015-06-28 22:13:16 +0800

    add more args to decorator

--- test/match.js
+++ test/match.js
@@ -12,7 +12,7 @@ var cgi = require('../lib/index')({
 describe('match', function (){
   it('file', function(done) {
     cgi.match('/scripts/export.sh', function (result) {
-      assert.equal(result.fpath, __dirname + '/scripts/export.sh');
+      assert.equal(result.fpath, '/scripts/export.sh');
       assert.equal(result.isDirectory, false);
       assert.equal(result.isFile, true);
       assert.equal(result.isExecutable, true);
@@ -23,7 +23,7 @@ describe('match', function (){
   });
   it('file non executable', function(done) {
     cgi.match('/self.js', function (result) {
-      assert.equal(result.fpath, __dirname + '/self.js');
+      assert.equal(result.fpath, '/self.js');
       assert.equal(result.isDirectory, false);
       assert.equal(result.isFile, true);
       assert.equal(result.isExecutable, false);
@@ -34,7 +34,7 @@ describe('match', function (){
   });
   it('directory', function(done) {
     cgi.match('/cgi', function (result) {
-      assert.equal(result.fpath, __dirname + '/cgi');
+      assert.equal(result.fpath, '/cgi');
       assert.equal(result.isDirectory, true);
       assert.equal(result.isFile, false);
       done();
@@ -42,7 +42,7 @@ describe('match', function (){
   });
   it('directory/ + has index', function(done) {
     cgi.match('/cgi/', function (result) {
-      assert.equal(result.fpath, __dirname + '/cgi/index.sh');
+      assert.equal(result.fpath, '/cgi/index.sh');
       assert.equal(result.isDirectory, false);
       assert.equal(result.isFile, true);
       done();
